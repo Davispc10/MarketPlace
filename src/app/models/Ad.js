b9e62de60f307fb27e41c9d8c21ba255d4ce9cf1
davispc10 <davispc100@gmail.com>
2019-07-31 22:34:35 -0300

    Adição de paginação usando mongoose-paginate

--- src/app/models/Ad.js
+++ src/app/models/Ad.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
 
 const Ad = new mongoose.Schema({
   title: {
@@ -23,5 +24,7 @@ const Ad = new mongoose.Schema({
     default: Date.now
   }
 })
+
+Ad.plugin(mongoosePaginate)
 
 module.exports = mongoose.model('Ad', Ad)
