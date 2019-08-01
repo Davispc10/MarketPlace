7be6bd1fc277fa4646115e22c0ab9d7b091eeac5
davispc10 <davispc100@gmail.com>
2019-07-31 22:27:50 -0300

    Criação de crud e rotas de mode Ad

---
+++ src/app/controllers/AdController.js
@@ -0,0 +1,37 @@
+const Ad = require('../models/Ad')
+
+class AdController {
+  async index (req, res) {
+    const ads = await Ad.find()
+
+    return res.json(ads)
+  }
+
+  async show (req, res) {
+    const ad = await Ad.findById(req.params.id)
+
+    return res.json(ad)
+  }
+
+  async store (req, res) {
+    const ad = await Ad.create({ ...req.body, author: req.userId })
+
+    return res.json(ad)
+  }
+
+  async update (req, res) {
+    const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    return res.json(ad)
+  }
+
+  async destroy (req, res) {
+    await Ad.findByIdAndDelete(req.params.id)
+
+    return res.send()
+  }
+}
+
+module.exports = new AdController()
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,24 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+const controllers = require('./app/controllers')
+// const UserController = require('./app/controllers/UserController')
+// const SessionController = require('./app/controllers/SessionController')
+
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
